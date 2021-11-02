7987df76cd23561c5226255b770df9dadc40affe
Ondra_Krejci <o.krejci13@gmail.com>
2021-11-02 20:23:42 +0100

    Added class handling ORP names

--- ciselniky.py
+++ ciselniky.py
@@ -1,4 +1,10 @@
 
+import csv
+
+from typing import Tuple, Union
+
+from download import DATA_PATH
+
 CSU7700 = {
     '400000600005000': '0-4 ',
     '400005610010000': '5-9',
@@ -42,3 +48,82 @@ NUTS3 = {
     'CZ072': 'Zlínský kraj',
     'CZ080': 'Moravskoslezský kraj'
 }
+
+class ORP:
+    def __init__(self) -> None:
+        self.nazvy, self.zkracene = ORP.get_orp_nazvy()
+
+    @staticmethod
+    def load_orp_zkracene() -> dict:
+        with open('%s/%s' % (DATA_PATH, 'orp-ciselnik.csv'), 'r', encoding='windows-1250') as file:
+            header = [
+                'KODJAZ',
+                'AKRCIS',
+                'KODCIS',
+                'orp_kod',
+                'orp_zkraceny_nazev',
+                'orp_nazev',
+                'ADMPLOD',
+                'ADMNEPO',
+                'KOD_RUIAN'
+            ]
+            reader = csv.DictReader(file, header)
+            reader.__next__() # header
+
+            orp_data = {}
+            for line in reader:
+                orp_data[int(line['orp_kod'])] = line['orp_zkraceny_nazev']
+
+            return orp_data
+
+    @staticmethod
+    def create_orp_kraje_vazba_json() -> dict:
+        orp_zkracene = ORP.load_orp_zkracene()
+        with open('%s/%s' % (DATA_PATH, 'vazba-orp-kraj-nuts.csv'), 'r', encoding='windows-1250') as file:
+            header = [
+                'KODJAZ',
+                'TYPVAZ',
+                'AKRCIS1',
+                'KODCIS1',
+                'orp_kod',
+                'orp_nazev',
+                'AKRCIS2',
+                'KODCIS2',
+                'kraj_kod',
+                'kraj_nazev'
+            ]
+            reader = csv.DictReader(file, header)
+            reader.__next__() # header
+
+            orp_data = {}
+            for line in reader:
+                orp_kod = int(line['orp_kod'])
+                orp_data[orp_kod] = {
+                    'nazev': line['orp_nazev'],
+                    'zkraceny_nazev': orp_zkracene[orp_kod],
+                    'kraj_kod': line['kraj_kod'],
+                    'kraj_nazev': line['kraj_nazev']
+                }
+
+            return orp_data
+
+    @staticmethod
+    def get_orp_nazvy() -> Tuple[dict, dict]:
+        orp_kraj_vazba = ORP.create_orp_kraje_vazba_json()
+
+        nazvy = {}
+        zkracene = {}
+        for orp_kod, data in orp_kraj_vazba.items():
+            nazvy[data['nazev']] = orp_kod
+            zkracene[data['zkraceny_nazev']] = orp_kod
+
+        return (nazvy, zkracene)
+
+    def get_orp_kod(self, nazev: str) -> Union[int, None]:
+        if nazev in self.nazvy:
+            return self.nazvy[nazev]
+        elif nazev in self.zkracene:
+            return self.zkracene[nazev]
+
+        return None
+
