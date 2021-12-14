f41bc7d0e10020feed1101f9b3a1a7efd489d085
OliverKunik <xkunik00@stud.fit.vutbr.cz>
2021-12-14 19:49:51 +0100

    Added B1 graph

--- plot_graphs.py
+++ plot_graphs.py
@@ -11,6 +11,8 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import scipy as sp
+from matplotlib.dates import date2num
+import datetime
 
 def plot_A1():
     df = pd.read_csv('data_csv/A1-covid_po_mesicich.csv', delimiter=";")
@@ -35,10 +37,23 @@ def plot_A2():
 
 def plot_B1():
     df = pd.read_csv('data_csv/B1-prirustky_kraj.csv', delimiter=";")
+    df['datum_zacatek'] = pd.to_datetime(df['datum_zacatek'])
+    mask = df['datum_zacatek'] == '2020-10-01'
+    df = df.loc[mask]
+    ax = plt.subplot(111)
+    N = 14
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.27
+    ax.bar(ind, df['kraj_populace'].to_numpy(), width=0.2, color='b', align='center')
+    ax.bar(ind + width, df['nakazeni_prirustek'].to_numpy(), width=0.2, color='r', align='center')
+    ax2 = ax.twinx()
+    ax2.plot(df['kraj_nazev'].to_numpy(), df['nakazeni_prirustek'].to_numpy()/df['kraj_populace'].to_numpy())
+    ax.set_xticklabels( df['kraj_nazev'].to_numpy() )
+    plt.show()
 
 
 
-plot_A1()
-plot_A2()
+#plot_A1()
+#plot_A2()
 plot_B1()
 
