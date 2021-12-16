46fb0a085130521ff9d8950493d6a242266e0286
Ondra_Krejci <o.krejci13@gmail.com>
2021-12-16 11:30:27 +0100

    Load/store methods for invalid orp codes

--- part1/invalid_orp.py
+++ part1/invalid_orp.py
@@ -7,6 +7,7 @@
 
 import json
 import sys
+import csv
 
 from typing import Union
 
@@ -42,6 +43,7 @@ class InvalidORPCodeDetector():
                 name_key = self.FILES[fname]['nazev']
             else:
                 print('Unknown file name %s' % fname, file=sys.stderr)
+                sys.exit(3)
 
         with open('%s/%s' % (DATA_PATH, fname), 'r', encoding='utf-8') as file:
             doc = json.load(file)['data']
@@ -57,8 +59,27 @@ class InvalidORPCodeDetector():
     def invalid_orp_set_to_dict(orp_set: set) -> dict:
         return dict((x[0], x[1]) for x in orp_set)
 
+    def save_invalid_orp_codes(self, fname: str, out_fname: str) -> None:
+        missing_orps = self.get_invalid_orp_set(fname)
+        with open(out_fname, 'w', encoding='utf-8', newline='') as out_file:
+            writer = csv.writer(out_file, delimiter=';')
+            writer.writerow(['orp_code', 'orp_name'])
+            for orp_name, orp_code in missing_orps:
+                writer.writerow([orp_code, orp_name])
+
+    def load_invalid_orp_dict(self, fname: str) -> dict:
+        missing_orps = {}
+        with open(fname, 'r', encoding='utf-8') as file:
+            reader = csv.DictReader(file, delimiter=';')
+            for line in reader:
+                missing_orps[line['orp_name']] = line['orp_code']
+
+        return missing_orps
+
 if __name__ == '__main__':
     detector = InvalidORPCodeDetector()
-    ORP_NAKAZENI = detector.get_invalid_orp_set('obce-nakazeni.json')
-    ORP_OCKOVANI = detector.get_invalid_orp_set('orp-ockovani-geografie.json')
-    ORP_HOSPITALIZOVANI = detector.get_invalid_orp_set('orp-nakazeni-hospitalizovani.json')
+    detector.save_invalid_orp_codes('orp-ockovani-geografie.json', '%s/%s' % (DATA_PATH, 'ockovani_invalid_orp.csv'))
+
+    #ORP_NAKAZENI = detector.get_invalid_orp_set('obce-nakazeni.json')
+    #ORP_OCKOVANI = detector.get_invalid_orp_set('orp-ockovani-geografie.json')
+    #ORP_HOSPITALIZOVANI = detector.get_invalid_orp_set('orp-nakazeni-hospitalizovani.json')
